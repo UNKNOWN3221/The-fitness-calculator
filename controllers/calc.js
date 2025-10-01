d35e350cd556ad44cf1842fd2aa35e492152b3f2
ahq_292 <ali.quraishy2014@email.com>
2025-10-01 16:07:52 +0300

    edit

--- controllers/calc.js
+++ controllers/calc.js
@@ -5,7 +5,7 @@ const user = require('../models/user')
 router.get('/', async (req, res) => {
   try {
     currentUser = await user.findById(req.session.user._id)
-    res.render('calc/index.ejs',{calc: currentUser.calc,});
+    res.render('calc/index.ejs',{calc: currentUser.calc,user: currentUser});
   } catch (error) {
     console.log(error);
     res.redirect('/');
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 
 router.get('/new', (req, res) => {
     try {
-        res.render('calc/new.ejs');
+        res.render('calc/new.ejs',{ user: req.session.user});
     } catch(error) {
         console.log(error)
     }
@@ -31,5 +31,41 @@ router.post('/', async (req,res)=> {
     res.redirect('/')
   }
 });
+router.get('/:calcId', async (req, res) => {
+  try {
+    const currentUser = await user.findById(req.session.user._id);
+    const record = currentUser.calc.id(req.params.calcId);
+    res.render('calc/show.ejs', { record, user: currentUser });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+router.delete('/:calcId', async (req,res) => {
+  try{
+    currentUser = await user.findById(req.session.user._id);
+    const record = currentUser.calc.id(req.params.calcId);
+    if (record){
+      record.deleteOne();
+      await currentUser.save()
+    }
+    res.redirect(`/users/${currentUser._id}/calc`)
+  }
+  catch (error){
+    console.log(error);
+    res.redirect('/')
+  }
+});
+router.get('/:calcId/edit',async (req, res) => {
+  try {
+    const currentUser = await user.findById(req.session.user._id);
+    const record = currentUser.calc.id(req.params.calcId);
+    res.render('calc/edit.ejs',{record: record},)
+  }
+  catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
 
 module.exports = router;
