79434c0e81be037e533715bf86508e6f2bb889c7
ahq_292 <ali.quraishy2014@email.com>
2025-10-02 13:22:23 +0300

    calculations and edit

--- controllers/calc.js
+++ controllers/calc.js
@@ -2,6 +2,50 @@ const express = require('express')
 const router = express.Router()
 const calc = require('../models/calc')
 const user = require('../models/user')
+
+// Helper functions
+
+const calculateBMI = (record) => {
+ 
+  return record.Weight/(record.Height/100)**2
+}
+const calcFatMen = (record) => {
+  const bmi= calculateBMI(record)
+  return 1.2*bmi+0.23*record.Age-16.2
+}
+const calcFatwomen = (record) => {
+  const bmi = calculateBMI(record)
+  return 1.2*bmi+0.23*record.Age-5.4
+}
+const calcCalories = (record) => {
+  const bmrMen = (10*record.Weight)+(6.25*record.Height)-(5*record.Age)+5
+  const bmrWomen = (10*record.Weight)+(6.25*record.Height)-(5*record.Age)-161
+  if (record.gender == 'male'){
+    if (record.activityLevel == 'sendentary'){
+      return bmrMen*1.2
+    }else if (record.activityLevel == 'light activity') {
+      return bmrMen*1.375
+    }else if (record.activityLevel == 'moderate activity') {
+      return bmrMen*1.55
+    }else if (record.activityLevel == 'very active') {
+      return bmrMen*1.725
+    }else if (record.activityLevel == 'extra active') {
+      return bmrMen*1.9
+    }
+  } else{
+      if (record.activityLevel == 'sedentary'){
+      return bmrWomen*1.2
+    }else if (record.activityLevel == 'light activity') {
+      return bmrWomen*1.375
+    }else if (record.activityLevel == 'moderate activity') {
+      return bmrWomen*1.55
+    }else if (record.activityLevel == 'very active') {
+      return bmrWomen*1.725
+    }else if (record.activityLevel == 'extra active') {
+      return bmrWomen*1.9
+    }
+  }
+}
 router.get('/', async (req, res) => {
   try {
     currentUser = await user.findById(req.session.user._id)
@@ -24,7 +68,7 @@ router.post('/', async (req,res)=> {
     currentUser = await user.findById(req.session.user._id);
     currentUser.calc.push(req.body);
     await currentUser.save();
-    res.redirect(`users/${currentUser._id}/calc`)
+    res.redirect(`/users/${currentUser._id}/calc`)
   }
   catch (error) {
     console.log(error)
@@ -35,7 +79,11 @@ router.get('/:calcId', async (req, res) => {
   try {
     const currentUser = await user.findById(req.session.user._id);
     const record = currentUser.calc.id(req.params.calcId);
-    res.render('calc/show.ejs', { record, user: currentUser });
+    const bmi = Number(calculateBMI(record).toFixed(1))
+    const fatMen = Number(calcFatMen(record).toFixed(1))
+    const fatWomen = Number(calcFatwomen(record).toFixed(1))
+    const calories = Number(calcCalories(record).toFixed(0))
+    res.render('calc/show.ejs', { record, user: currentUser, bmi, fatMen,fatWomen,calories });
   } catch (error) {
     console.log(error);
     res.redirect('/');
--- models/calc.js
+++ models/calc.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose')
 
 const calcSchema = new mongoose.Schema({
-    Name:{type:String, required:true},
     Age:{type:Number, required:true},
     Height:{type:Number, required:true},
     Weight:{type:Number, required:true},
     activityLevel:{
         type:String,
-        enum:['sendentary', 'light activity','moderate activity', 'very active', 'extra active']
+        enum:['sedentary', 'light activity','moderate activity', 'very active', 'extra active'],
+        required:true
     },
     gender:{
         type: String,
         enum:['male','female']
     },
+    createdAt: { type: Date, default: Date.now },
 });
 const Calc = mongoose.model('calc', calcSchema)
 module.exports = { Calc, calcSchema };
